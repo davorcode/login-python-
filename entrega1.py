38cb0b0fd85939252bd55bd483f1b0a1c164422c
Davor Plisich <plisichdavor@gmail.com>
2025-11-20 19:59:59 -0300

    Actualizo archivo con corrección de variables globales y validación

--- entrega1.py
+++ entrega1.py
@@ -1,24 +1,26 @@
-Usuarios = {}
+USUARIOS = {}
 
 def registro (): 
     nombre = input ("ingresa tu nombre")
+    while nombre.strip () == "" or nombre in USUARIOS:
+        nombre = input ("el nombre está vacio o ya existe, ingresá otro")
     contraseña = input ("ingresa tu contraseña")
     while contraseña.strip() == "":
         contraseña = input ("contraseña incorrecta, escribela de vuelta")
-    Usuarios[nombre] = contraseña 
+    USUARIOS[nombre] = contraseña 
 
 def mostrar_usuarios ():
-    if not Usuarios: 
+    if not USUARIOS: 
         print ("no hay usuarios registrados")
         return
-    for usuario, contraseña in Usuarios.items():
+    for usuario, contraseña in USUARIOS.items():
         print(usuario, contraseña)
 
 def login ():
     nombre = input ("ingresa tu nombre correspondiente")
     contraseña = input ("ingresa tu contraseña")
-    if nombre in Usuarios:
-        if  contraseña == Usuarios[nombre]:
+    if nombre in USUARIOS:
+        if  contraseña == USUARIOS[nombre]:
             print ("bienvenido")
         else: 
             print ("contraseña incorrecta")
